109d68201f54f127101627b3bef64eed9019431e
Will Barbosa <wildemar.barbosa@outlook.com>
2018-10-26 07:02:35 +0000

    Adicionado as rotas home e painel

---
+++ app/controllers/painel_controller.rb
@@ -0,0 +1,9 @@
+class PainelController < ApplicationController
+  before_action :authenticate_user!
+    
+  def index
+    @user = current_user
+  	@session = user_session
+  	@authenticate = user_signed_in?
+  end
+end
