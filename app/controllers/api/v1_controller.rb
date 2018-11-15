ac3cc801467e19e43a5a002b5b2b6cdac54cb3e4
Will Barbosa <wildemar.barbosa@outlook.com>
2018-11-15 19:10:55 +0000

    Controller Api v1, metodos create, destroy e rotas incluídos

--- app/controllers/api/v1_controller.rb
+++ app/controllers/api/v1_controller.rb
@@ -3,24 +3,42 @@ class Api::V1Controller < ApplicationController
   
   def pairs
     @pairs = @user.pairs
-    render json: @pairs
+    if(@pairs.length == 0)
+      render json: {data: @pairs}, status: 204
+    else
+      render json: {data: @pairs}, status: 200
+    end
   end
   
   def show
     if Pair.find(params[:id]).user_id == @user.id
-      @pair = @user.pairs.find(params[:id])
-      render json: @pair
+      @pairs = @user.pairs.find(params[:id])
+      render json: @pairs, status: 200
     else
-      render json: []
+      render json: [], status: 404
     end
   end
   
   def create
     @pair = Pair.new(pair_params)
     if @pair.save
-      render json: {message: 'Adicionado com sucesso!'}, status: 200
+      render json: {data: 'Adicionado com sucesso!'}, status: 201
     else
-      render json: {message: 'Erro ao inserir na base de dados.'}, status: 400
+      render json: {data: 'Erro ao inserir na base de dados.'}, status: 400
+    end
+  end
+  
+  def destroy
+    if Pair.find(params[:id]).user_id == @user.id
+      @pair = Pair.find(params[:id])
+      pair_name = @pair.pair
+        if @pair.destroy
+          render json: {data: "Item #{pair_name} removido com sucesso!"}, status: 200
+        else
+          render json: {data: 'Não foi possível remover este item'}, status: 400
+        end
+    else
+      render json: [], status: 404
     end
   end
   
@@ -30,6 +48,11 @@ class Api::V1Controller < ApplicationController
   end
   
   def set_user
-    @user = current_user
+    @authenticate = user_signed_in?
+    if @authenticate
+      @user = current_user
+    else
+      render json: {data: 'Você precisa estar autenticado para realizar esta ação'}, status: 401
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     get 'v1/pairs' => 'v1#pairs', as: 'api_v1_pairs'
     get 'v1/pairs/:id' => 'v1#show', as: 'api_v1_show'
+    post 'v1/pairs' => 'v1#create', as: 'api_v1_create'
+    delete 'v1/pairs/:id' => 'v1#destroy', as: 'api_v1_destroy'
   end
   get '/config/pair', :to => 'pair#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
