444b5ff66968e48788f3682b2fe03cef59ffa340
Will Barbosa <wildemar.barbosa@outlook.com>
2018-11-23 03:34:21 +0000

    Compilando para prod

--- test/controllers/apiv1_controller_test.rb
+++ test/controllers/apiv1_controller_test.rb
@@ -1,9 +1,3 @@
 require 'test_helper'
 
-class Apiv1ControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get apiv1_index_url
-    assert_response :success
-  end
 
-end
