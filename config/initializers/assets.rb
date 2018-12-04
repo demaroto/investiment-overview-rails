beb388aae6177ef31493a15eb7bcf65dc2a3504a
Will Barbosa <wildemar.barbosa@outlook.com>
2018-12-04 08:01:40 +0000

    Lucas Santos - teste 9

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,10 +7,10 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-Rails.application.config.assets.precompile += %w(jQuery.js)
-Rails.application.config.assets.precompile += %w(application.js)
-Rails.application.config.assets.precompile += %w(particles.js)
-Rails.application.config.assets.precompile += %w(app.js)
+# Rails.application.config.assets.precompile += %w(jQuery.js)
+# Rails.application.config.assets.precompile += %w(application.js)
+# Rails.application.config.assets.precompile += %w(particles.js)
+# Rails.application.config.assets.precompile += %w(app.js)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
